46683e1ff587dd816a36d77bb69d1bc61e795910
HaoPNT0805 <edwardpaul9350@gmail.com>
2025-03-17 05:45:23 +0700

    chapter 4

---
+++ learning4/video4.js
@@ -0,0 +1,11 @@
+console.log('Hello World with JS')
+// let a = 'Tôi là string - chuỗi'
+let a = `I'm a string`
+let text = `We are the so-called "Vikings" from the north.`; //dấu backtick ``
+
+// console.log(a)
+// console.log(4 + 5 + 'a') // 9a số nguyên + chuỗi là ra số cộng chuỗi
+
+// console.log('a' + 5 + 4) //a54 nó hiểu chuỗi + với 1 chuỗi khác
+console.log(text);
+console.log(a);
