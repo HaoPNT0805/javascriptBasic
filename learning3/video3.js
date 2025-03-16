7788bde64691d7cb97994ba7b6a7500f63716d20
HaoPNT0805 <edwardpaul9350@gmail.com>
2025-03-16 22:55:13 +0700

    chapter 3

---
+++ learning3/video3.js
@@ -0,0 +1,13 @@
+// let c = 5;
+// let d = 5.9;
+
+// let x = 5; 
+// let y = 5; 
+// let z = 6;
+
+
+console.log(`Hello " " ' ' World from Javascript: `)
+let x = 5, y = 6, z = 7;
+
+console.log("Test string", 'x = ', x, 'y = ', y, 'z = ', z);
+console.log("Test string 222: ",  'x = ' + x  + 'y = ' + y + 'z = ' + z);
