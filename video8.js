104fad37f1a96c754f3a5be093e79c32fca46eb1
HaoPNT0805 <edwardpaul9350@gmail.com>
2025-03-17 08:20:10 +0700

    Chapter 8 about obj

---
+++ video8.js
@@ -0,0 +1,11 @@
+console.log('Hello world with Javascript')
+let a = {
+    name: 'Eric', 
+    address: 'Ha Noi'
+};
+
+let b = 'Eric';
+let c = '';
+console.log('type of a: ', typeof a, 'type of b: ', typeof b)
+console.log(`What is your name ? `, `My name 's : `, a.name);
+console.log(`Where are you from ? `, `I'm from: `, a.address);
