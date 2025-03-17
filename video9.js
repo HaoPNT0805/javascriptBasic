58ae33c7870ef4ac1a0cccd648b195b1c8f68cc8
HaoPNT0805 <edwardpaul9350@gmail.com>
2025-03-17 20:15:22 +0700

    end course basic

---
+++ video9.js
@@ -0,0 +1,16 @@
+console.log('Hello world with Javascript')
+let a = {
+    name: 'Paul',
+    age: 20
+}; //key: value
+console.log('check data object: ', a)
+console.log(`my name's: `, a.name)
+
+//{} : này là khai báo object
+//[] array
+let n = 4;
+let sum = 0;
+for(let i = 0; i <= n; i++){
+    sum += i;
+}
+console.log("Sum all number: " + sum);
